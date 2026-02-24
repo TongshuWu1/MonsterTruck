37c96689a97cb56bef6a17a67b26691432092859
TongshuWu <tow225@lehigh.edu>
2026-02-24 09:07:18 -0500

    suhhsssatest

--- PLSSGP_compare/engine/envs/mountaincar_hold/adapter.py
+++
@@ -1,3 +0,0 @@
-# Backward-compatibility adapter. Prefer using envs/mountaincar_hold/env.py directly.
-from envs.mountaincar_hold.env import *  # noqa: F401,F403
-from .task import MountainCarHoldTask
--- PLSSGP_compare_test/worker_single_run.py
+++
@@ -1,4 +0,0 @@
-from worker_method_run import main
-
-if __name__ == "__main__":
-    main()
